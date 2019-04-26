2ea0aac54db70698278d136dac733692ed1868f6
Guido83 <guido.welsch@gmail.com>
2019-04-26 08:31:13 +0200

    Update havoc.mk

--- havoc.mk
+++ havoc.mk
@@ -1,3 +1,4 @@
 $(call inherit-product, vendor/havoc/config/common.mk)
+$(call inherit-product, vendor/havoc/config/telephony.mk)
 $(call inherit-product, device/havoc/sepolicy/common/sepolicy.mk)
 -include vendor/havoc/build/core/config.mk
