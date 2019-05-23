dd25438d691211981e6e3b98bd8421f4499ed9ce
Guido83 <guido.welsch@gmail.com>
2019-05-23 09:47:41 +0200

    Update bootleggers.mk

--- bootleggers.mk
+++ bootleggers.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 $(call inherit-product, vendor/bootleggers/config/telephony.mk)
+$(call inherit-product, device/bootleg/sepolicy/common/sepolicy.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 -include vendor/bootleggers/build/core/config.mk
