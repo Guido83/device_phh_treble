142e3a8247ca805eb17368f08c548807669479ad
Guido83 <guido.welsch@gmail.com>
2019-05-23 08:21:55 +0200

    Create bootleggers.mk

---
+++ bootleggers.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/telephony.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+-include vendor/bootleggers/build/core/config.mk
